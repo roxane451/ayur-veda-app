c5a1515d5fa29c3ab847c2325a438affee8427e0
roxane451 <35878565+roxane451@users.noreply.github.com>
2026-04-22 08:13:00 +0000

    fix(backend):delete mongo import

--- backend/src/types/migrate-mongo.d.ts
+++ backend/src/types/migrate-mongo.d.ts
@@ -1,7 +1,8 @@
 declare module 'migrate-mongo' {
-  import { Db, MongoClient } from 'mongodb';
-
-  export function up(db: Db, client: MongoClient): Promise<string[]>;
-  export function down(db: Db, client: MongoClient): Promise<string[]>;
-  export function status(db: Db): Promise<Array<{ fileName: string; appliedAt: string }>>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export function up(db: any, client: any): Promise<string[]>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export function down(db: any, client: any): Promise<string[]>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export function status(db: any): Promise<Array<{ fileName: string; appliedAt: string }>>;
 }
